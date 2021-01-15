c201588b761881883478a1b1cc5e6e4108849556
HiMyNameIsAdam <xgurke@gmail.com>
2021-01-15 16:23:22 +0100

    Shooting started - disgusting code requires refactoring

--- lib/game.dart
+++ lib/game.dart
@@ -10,12 +10,15 @@ class BoxGame extends Game with TapDetector{
   bool left = false;
   Vector2 pos = Vector2(0, 0);
   Sprite player;
+  int ctr = 0;
+  List<Sprite> objects = [];
+  List<Vector2> positions = [];
 
   Future<void> onLoad() async{
     await images.load('ship_test.png'); 
+    await images.load('laserRed01.png'); 
     pos = Vector2(size.x/2 - 50, size.y-100);
     player = Sprite(images.fromCache('ship_test.png'));
-
   }
 
   void render(Canvas canvas) {
@@ -25,10 +28,31 @@ class BoxGame extends Game with TapDetector{
     bgPaint.color = Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
     // Ship
-    player.render(canvas, position: Vector2(pos.x, size.y-100), size: Vector2(100, 100));
+    player.render(canvas, position: Vector2(pos.x, size.y-50), size: Vector2(50, 50));
+    print(objects.length);
+    for (int i = 0; i < objects.length; i++) {
+      objects[i].render(canvas, position: positions[i], size: Vector2(9, 54));
+    }
   }
 
   void update(double t) {
+    ctr += 1;
+    List<int> todel = [];
+    for (int i = 0; i < objects.length; i++) {
+      positions[i].y -= 10;
+      if (positions[i].y <= -54){
+        todel.add(i);
+      }
+    }
+    for (int i = todel.length-1; i >= 0; i--){
+      positions.removeAt(i);
+      objects.removeAt(i);
+    }
+    if (ctr >= 10) {
+      ctr = 0;
+      objects.add(new Sprite(images.fromCache('laserRed01.png')));
+      positions.add(Vector2(pos.x+25, pos.y));
+    }
     if (left){
       pos.x -= 5;
     } else {
