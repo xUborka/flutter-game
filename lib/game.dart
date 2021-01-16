76c13fe8c922ad18585b34413010ded8fe8545ac
Gergely Bakos <geryllaz@hotmail.com>
2021-01-16 10:35:59 +0100

    - #11 movement update

--- lib/game.dart
+++ lib/game.dart
@@ -5,19 +5,20 @@ import 'package:flame/gestures.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/gestures.dart';
 
-class BoxGame extends Game with TapDetector{
+class BoxGame extends Game with TapDetector {
   Color clr = Color(0xffffffff);
-  bool left = false;
+  bool isLeft = false;
+  bool isTouching = false;
   Vector2 pos = Vector2(0, 0);
   Sprite player;
   int ctr = 0;
   List<Sprite> objects = [];
   List<Vector2> positions = [];
 
-  Future<void> onLoad() async{
-    await images.load('ship_test.png'); 
-    await images.load('laserRed01.png'); 
-    pos = Vector2(size.x/2 - 50, size.y-100);
+  Future<void> onLoad() async {
+    await images.load('ship_test.png');
+    await images.load('laserRed01.png');
+    pos = Vector2(size.x / 2 - 50, size.y - 100);
     player = Sprite(images.fromCache('ship_test.png'));
   }
 
@@ -28,7 +29,8 @@ class BoxGame extends Game with TapDetector{
     bgPaint.color = Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
     // Ship
-    player.render(canvas, position: Vector2(pos.x, size.y-50), size: Vector2(50, 50));
+    player.render(canvas,
+        position: Vector2(pos.x, size.y - 50), size: Vector2(50, 50));
     print(objects.length);
     for (int i = 0; i < objects.length; i++) {
       objects[i].render(canvas, position: positions[i], size: Vector2(9, 54));
@@ -40,39 +42,45 @@ class BoxGame extends Game with TapDetector{
     List<int> todel = [];
     for (int i = 0; i < objects.length; i++) {
       positions[i].y -= 10;
-      if (positions[i].y <= -54){
+      if (positions[i].y <= -54) {
         todel.add(i);
       }
     }
-    for (int i = todel.length-1; i >= 0; i--){
+    for (int i = todel.length - 1; i >= 0; i--) {
       positions.removeAt(i);
       objects.removeAt(i);
     }
     if (ctr >= 10) {
       ctr = 0;
       objects.add(new Sprite(images.fromCache('laserRed01.png')));
-      positions.add(Vector2(pos.x+25, pos.y));
+      positions.add(Vector2(pos.x + 25, pos.y));
     }
-    if (left){
-      pos.x -= 5;
-    } else {
-      pos.x += 5;
-    }
-    if (pos.x < 0){
-      pos.x = 0;
-    }
-    if (pos.x > size.x-100){
-      pos.x = size.x-100;
+    if (isTouching) {
+      if (isLeft) {
+        pos.x -= 5;
+      } else {
+        pos.x += 5;
+      }
+      if (pos.x < 0) {
+        pos.x = 0;
+      }
+      if (pos.x > size.x - 100) {
+        pos.x = size.x - 100;
+      }
     }
   }
 
   void onTapDown(TapDownDetails d) {
+    isTouching = true;
     double screenCenterX = size.x / 2;
     if (d.globalPosition.dx < screenCenterX) {
-      left = true;
+      isLeft = true;
     } else {
-      left = false;
+      isLeft = false;
     }
-}
+  }
 
+  void onTapUp(TapUpDetails tapUpDetails) {
+    isTouching = false;
+  }
 }
