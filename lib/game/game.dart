f5978f085b1adc2b1c8c101cf8e04f79f06eef43
Gergely Bakos <geryllaz@hotmail.com>
2021-02-02 19:16:33 +0100

    - blast2 sfx

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -7,6 +7,7 @@ import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_game/game/components/hud.dart';
 import 'package:flutter_game/game/game_assets.dart';
+import 'package:flutter_game/managers/audio_manager.dart';
 import 'package:flutter_game/sprites/enemy.dart';
 import 'package:flutter_game/sprites/projectile.dart';
 import 'package:flutter_game/sprites/ship.dart';
@@ -74,14 +75,13 @@ class SpaceGame extends BaseGame with TapDetector {
       projectiles.removeAt(todelProjectiles[i]);
     }
     if (ctr % 25 == 0) {
-      projectiles.add(Projectile(MiscSprites.projectile(),
-          Position(ship.x + (ctr % 50 == 0 ? 12 : 30), ship.y - 10)));
+      projectiles.add(Projectile(MiscSprites.projectile(), Position(ship.x + (ctr % 50 == 0 ? 12 : 30), ship.y - 10)));
+      AudioManager.blast2Sfx();
     }
     if (ctr >= 50) {
       score += 1;
       Enemy tEnemy = Enemy(EnemySprites.blueEnemy());
-      tEnemy.setByPosition(Position(
-          min(Random().nextDouble() * size.width, size.width - 50), -32));
+      tEnemy.setByPosition(Position(min(Random().nextDouble() * size.width, size.width - 50), -32));
       enemies.add(tEnemy);
       ctr = 0;
     }
@@ -100,8 +100,7 @@ class SpaceGame extends BaseGame with TapDetector {
       for (int j = 0; j < enemies.length; j++) {
         final Projectile crntProjectile = projectiles[i];
         final Enemy crntEnemy = enemies[j];
-        final Offset peakPoint = Offset(
-            crntProjectile.x + crntProjectile.width / 2.0, crntProjectile.y);
+        final Offset peakPoint = Offset(crntProjectile.x + crntProjectile.width / 2.0, crntProjectile.y);
         if (crntEnemy.toRect().contains(peakPoint)) {
           todelEnemy.add(j);
           todelProjectile.add(i);
