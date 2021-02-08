d8646113a914f1e96ef82f2e317a6e3d9a99af8d
HiMyNameIsAdam <xgurke@gmail.com>
2021-02-08 23:04:34 +0100

    Cleaning, testing animations

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,8 @@
+import 'package:flame/components/animation_component.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
+import 'package:flame/position.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_game/game/components/hud.dart';
 import 'package:flutter_game/game/game_assets.dart';
@@ -10,26 +13,28 @@ import 'components/background.dart';
 class SpaceGame extends BaseGame with TapDetector {
   Hud hud;
   Background bg;
-  
+  AnimationComponent player;
   SpaceGame() {
     add(bg = Background());
+    add(player = AnimationComponent(50, 50, HeartSprites.idle())..setByPosition(Position(100, 100)));
+    add(AnimationComponent(50, 50, HeartSprites.run())..setByPosition(Position(150, 100)));
     add(hud = Hud());
   }
 
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-  }
+  // @override
+  // void render(Canvas canvas) {
+  //   super.render(canvas);
+  // }
 
-  @override
-  void update(double dt) {
-    super.update(dt);
-  }
+  // @override
+  // void update(double dt) {
+  //   super.update(dt);
+  // }
 
   @override
   void onTapDown(TapDownDetails details) {
-    if (details.globalPosition.dx < size.width / 2.0) {
-    }
+    // if (details.globalPosition.dx < size.width / 2.0) {
+    // }
   }
 
   @override
--- lib/game/game_assets.dart
+++ lib/game/game_assets.dart
@@ -1,13 +1,18 @@
+import 'package:flame/animation.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/spritesheet.dart';
 import 'package:flame/sprite.dart';
 
 class GameAssets {
   static SpriteSheet _hearts;
+  static SpriteSheet _player;
+  static Animation _playerIdle;
+  static Animation _playerRunning;
 
   static Future<void> load() async {
     await Flame.images.loadAll([
       'HealthBar/heart-tileset-01.png',
+      'among-us-01-Sheet.png',
     ]);
     _hearts = SpriteSheet(
         imageName: 'HealthBar/heart-tileset-01.png',
@@ -15,6 +20,21 @@ class GameAssets {
         textureWidth: 32,
         columns: 2,
         rows: 2);
+    _player = SpriteSheet(
+        imageName: 'among-us-01-Sheet.png',
+        textureHeight: 32,
+        textureWidth: 32,
+        columns: 6,
+        rows: 1);
+    _playerIdle = Animation.spriteList(
+        [_player.getSprite(0, 0), _player.getSprite(0, 1)],
+        stepTime: .2);
+    _playerRunning = Animation.spriteList([
+      _player.getSprite(0, 2),
+      _player.getSprite(0, 3),
+      _player.getSprite(0, 4),
+      _player.getSprite(0, 5)
+    ], stepTime: .15);
   }
 }
 
@@ -24,4 +44,7 @@ class HeartSprites {
 
   static Sprite heartFull() => GameAssets._hearts.getSprite(1, 0);
   static Sprite heartHalf() => GameAssets._hearts.getSprite(1, 1);
+  static Animation idle() => GameAssets._playerIdle;
+  static Animation run() => GameAssets._playerRunning;
+  // Sprite.fromImage();
 }
